0a71266cfc139a37f24eb732a9110b2e7dd34f7c
mattmatt91 <matthias_muhr@web.de>
2022-11-13 17:32:16 +0100

    _

--- api/api.py
+++ api/api.py
@@ -17,10 +17,6 @@ def set_value(feature, value):
     val = random.randint(0,10)
     return {"data":val}
 
-@app.route('/time')
-def get_current_time():
-    print('time')
-    return {'time': time.time()}
 
 if __name__ == '__main__':
     app.run(debug=False,host= '127.0.0.1', port=5000)
