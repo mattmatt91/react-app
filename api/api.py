e061477126a7775802112a059178da186128a220
h4sh3 <pinkdaniel@web.de>
2022-11-14 18:34:21 +0100

    update api

--- api/api.py
+++ api/api.py
@@ -1,22 +1,40 @@
 import time
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import random
 
 app = Flask(__name__)
 
-@app.route('/features/')
+
+@app.route('/features')
 def get_features():
-    data = {'test1': time.time(), 'test2': 2342, 'test3': 'test'}
+    data = {
+        'brigthness': 1,
+        'freqarea': 12,
+        'smooth': 13,
+        'falldown': 14,
+        'fadespeed': 15,
+        'rainbow': 16,
+        'yrainbow': 17,
+        'sym': 18,
+        'maxdot': 19,
+        'blackspec': 10,
+        'center': False,
+        'animation': 12,
+        'render_spec': 12,
+        'render_animation': 11,
+        'quit': 11
+    }
     print('get freatures', data)
 
     return data
 
-@app.route('/feature/<feature>/<value>', methods=['POST', 'GET'])
-def set_value(feature, value):
-    print(value, feature, 'test setter')
-    val = random.randint(0,10)
-    return {"data":val}
+
+@app.route('/update_feature', methods=['POST'])
+def set_value():
+    print(request.json)
+    #val = random.randint(0, 10)
+    return {"data": ""}
 
 
 if __name__ == '__main__':
-    app.run(debug=False,host= '127.0.0.1', port=5000)
+    app.run(debug=False, host='127.0.0.1', port=5000)
