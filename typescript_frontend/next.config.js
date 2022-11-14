b9dffe0c08fc1b88cc2cc0f08d47aa0f825e2db0
h4sh3 <pinkdaniel@web.de>
2022-11-14 18:34:43 +0100

    add features component

--- typescript_frontend/next.config.js
+++ typescript_frontend/next.config.js
@@ -33,6 +33,14 @@ const nextConfig = {
 
     return config;
   },
+  rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `http://localhost:5000/:path*`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
