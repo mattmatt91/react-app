69b7ebe8e0a599ea5eda5e95b1fccd6ffebb3919
mattmatt91 <matthias_muhr@web.de>
2022-11-18 15:06:07 +0100

    _

--- typescript_frontend/src/pages/dots.tsx
+++ typescript_frontend/src/pages/dots.tsx
@@ -5,29 +5,32 @@ import { ChangeEvent, useEffect, useState } from "react"
 
 
 interface ComponentProps {
-	x: number
-	y: number
+	pos: number[]
+	color?: number[]
+	size?: number[]
 }
 
 let x = 50;
-	const y = 50;
-  
+const y = 50;
+
 const YourComponent: React.FC<ComponentProps> = (props: ComponentProps) => {
-	
+
 
 	//See annotations in JS for more information
 	const setup = (p5: p5Types, canvasParentRef: Element) => {
 		p5.createCanvas(500, 500).parent(canvasParentRef);
 	};
-
-	const draw = (p5: p5Types) => {
-		p5.background(0);
-		p5.ellipse(props.x, props.y, 70, 70);
-	};
-
-	return <Sketch setup={setup} draw={draw} />;
+	// 
+	 const draw = (p5: p5Types) => {
+	 	p5.background(0);
+	 	p5.ellipse(props.pos[0], props.pos[1], 10, 10);
+	 };
+	return <Sketch setup={setup} draw={draw} />
 };
 
 export default function ExporterTest() {
-    return <YourComponent x={5} y={5} />
+	return <div key='test'>
+		<YourComponent pos={[100,200]} />
+			</div>
+
 }
