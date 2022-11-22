cdd6e60f99f1e6932d6453efd8830845203dcc42
mattmatt91 <matthias_muhr@web.de>
2022-11-22 17:51:01 +0100

    some tests

--- api/api.py
+++ api/api.py
@@ -1,4 +1,4 @@
-import time
+from time import time
 from flask import Flask, jsonify, request
 import random
 
@@ -8,20 +8,21 @@ app = Flask(__name__)
 @app.route('/features')
 def get_features():
     data = {
-        'brigthness': {'val':1, 'options':[0,100]},# für int und float sind das die min und max werte
-        'freqarea': {'val':12, 'options':[10,50]},
-        'smooth': {'val':12, 'options':[10,50]},
-        'falldown': {'val':12, 'options':[10,50]},
-        'fadespeed': {'val':12, 'options':[10,50]},
-        'rainbow': {'val':12, 'options':[10,50]},
-        'yrainbow': {'val':62, 'options':[10,50]},
-        'sym': {'val':14, 'options':[10,50]},
-        'maxdot': {'val':12, 'options':[10,50]},
-        'blackspec':  {'val':True, 'options':[True, False]},
-        'center': {'val':12, 'options':[10,50]},
-        'animation':  {'val':'prog2', 'options':['prog1', 'prog2', 'prog3']},
-        'render_spec':  {'val':True, 'options':[True, False]},
-        'render_animation': {'val':True, 'options':[True, False]}
+        # für int und float sind das die min und max werte
+        'brigthness': {'val': 1, 'options': [0, 100]},
+        'freqarea': {'val': 12, 'options': [10, 50]},
+        'smooth': {'val': 12, 'options': [10, 50]},
+        'falldown': {'val': 12, 'options': [10, 50]},
+        'fadespeed': {'val': 12, 'options': [10, 50]},
+        'rainbow': {'val': 12, 'options': [10, 50]},
+        'yrainbow': {'val': 62, 'options': [10, 50]},
+        'sym': {'val': 14, 'options': [10, 50]},
+        'maxdot': {'val': 12, 'options': [10, 50]},
+        'blackspec':  {'val': True, 'options': [True, False]},
+        'center': {'val': 12, 'options': [10, 50]},
+        'animation':  {'val': 'prog2', 'options': ['prog1', 'prog2', 'prog3']},
+        'render_spec':  {'val': True, 'options': [True, False]},
+        'render_animation': {'val': True, 'options': [True, False]}
 
     }
     print('get freatures', data)
@@ -36,6 +37,13 @@ def set_value():
     val = data['value']
     print({feature: val})
     return {feature: val}
+
+
+@app.route('/get_time', methods=['GET', 'POST'])
+def get_time():
+    data = {"time": time()}
+    print(data)
+    return data
 
 
 if __name__ == '__main__':
--- typescript_frontend/src/pages/dots.tsx
+++ typescript_frontend/src/pages/dots.tsx
@@ -1,36 +1,28 @@
-import React from "react";
-import Sketch from "react-p5";
-import p5Types from "p5"; //Import this for typechecking and intellisense
-import { ChangeEvent, useEffect, useState } from "react"
+import React, { Component } from 'react';
 
 
-interface ComponentProps {
-	pos: number[]
-	color?: number[]
-	size?: number[]
-}
-
-let x = 50;
-const y = 50;
-
-const YourComponent: React.FC<ComponentProps> = (props: ComponentProps) => {
-
-
-	//See annotations in JS for more information
-	const setup = (p5: p5Types, canvasParentRef: Element) => {
-		p5.createCanvas(500, 500).parent(canvasParentRef);
+type MyProps = {
+	// using `interface` is also ok
+	message: string;
+  };
+  type MyState = {
+	count: number; // like this
+  };
+  
+ 
+ export default class App extends React.Component<{ message: string }, { count: number }> {
+	state = { count: 0 };
+	render() {
+	  return (
+		<div onClick={() => this.increment(1)}>
+		  {this.props.message} {this.state.count}
+		</div>
+	  );
+	}
+	increment = (amt: number) => {
+	  // like this
+	  this.setState((state) => ({
+		count: state.count + amt,
+	  }));
 	};
-	// 
-	 const draw = (p5: p5Types) => {
-	 	p5.background(0);
-	 	p5.ellipse(props.pos[0], props.pos[1], 10, 10);
-	 };
-	return <Sketch setup={setup} draw={draw} />
-};
-
-export default function ExporterTest() {
-	return <div key='test'>
-		<YourComponent pos={[100,200]} />
-			</div>
-
-}
+  }
